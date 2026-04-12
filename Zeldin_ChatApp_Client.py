ddb050d5d56c5dd3aad777411fb1e9cbb76950f2
Zachary Zeldin <47404563+zacharyz1234@users.noreply.github.com>
2026-04-11 21:01:49 -0400

    Added my client code

--- Zeldin_ChatApp_Client.py
+++ Zeldin_ChatApp_Client.py
@@ -1,1 +1,55 @@
+import socket as s
+
+from NetworksProjects.ChatApp.Zeldin_ChatApp_Server import connection_socket
+
+server_ip = input("Enter an IP Address (Hit enter for localhost): ")
+if(len(server_ip) == 0):
+    server_ip = 'localhost'
+
+
+# Infinite while loop that cycles until a valid server port is accepted
+while True:
+    try:
+        server_port = input("Please enter a valid port number: ")
+        if(server_port == ''):
+            server_port = 8080
+            break
+        # Port is input as a string so code has to typecast to check if the
+        # number is valid. Also checks to make sure the port number is between
+        # all valid port numbers
+        elif(int(server_port) > 0 and int(server_port) < 65535):
+            # Convert server port number to an int since it is entered
+            # as a string
+            server_port = int(server_port)
+            break
+    except ValueError:
+        print("That is not a valid port number.")
+
+
+
+client_username = input("Enter your username: ")
+
+#Creates a client socket
+client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+#Connects the client socket to the server
+client_socket.connect((server_ip, server_port))
+server_username = client_socket.recv(2048).decode()
+client_socket.send(client_username.encode())
+print("Connection accepted. Send the first message: ")
+
+while True:
+
+    client_message = input()
+
+    if(client_message != 'end'):
+        client_message = client_username + ': ' + client_message
+        print(client_message)
+        client_socket.send(client_message.encode())
+    else:
+        print(client_message)
+
+
+    server_message = client_socket.recv(2048).decode()
+    print(server_message)
 
